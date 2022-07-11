31a8a38ee82f989a25486162a18f85449a4573f0
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-11 19:03:04 +0300

    Human class complited

--- Human.java
+++ Human.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Human {
+import java.util.ArrayList;
+
+public class Human {
+    ArrayList<Pet> pets = new ArrayList<>();
+    public Human(ArrayList<Pet> humansPets) {
+        for (Pet pet: humansPets) {
+            pets.add(pet);
+        }
+    }
+    void call() {
+        System.out.println("*calling*");
+        for (Pet pet: pets) {
+            pet.voice();
+        }
+    }
 }
