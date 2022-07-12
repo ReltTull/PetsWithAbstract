e5ac9daba5269d0b8fa9724ba2478fee0b41b45b
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-12 13:43:09 +0300

    upd

--- Program.java
+++ Program.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Program {
     public static void main(String[] args) {
         ArrayList<Pet> humansPets = new ArrayList<>();
-        humansPets.add(new Cat());
-        humansPets.add(new Dog());
+        humansPets.add(new Cat(20));
+        humansPets.add(new Dog(10));
 
         new Human(humansPets).call();
     }
