15abbf63ab6c041e81ed107b9777788d3566ba1a
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-14 12:42:50 +0300

    + sort

--- Pet.java
+++ Pet.java
@@ -1,3 +1,12 @@
-abstract class Pet {
+import java.util.Iterator;
+
+abstract class Pet implements Comparable<Pet>{
+    int distance;
     abstract void voice();
+
+    abstract void getInfo();
+
+    public int compareTo(Pet p) {
+        return Integer.compare(this.distance, p.distance);
+    }
 }
