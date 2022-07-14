770c97c4c86054a04af1a21318bf3d30048e7cbe
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-14 12:43:03 +0300

    upd

--- Program.java
+++ Program.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class Program {
     public static void main(String[] args) {
         ArrayList<Pet> humansPets = new ArrayList<>();
+        humansPets.add(new Dog(15));
         humansPets.add(new Cat(20));
         humansPets.add(new Dog(10));
+        humansPets.add(new Cat(5));
 
         new Human(humansPets).call();
+
+        Collections.sort(humansPets);
+
+        Iterator<Pet> iter = humansPets.iterator();
+        while (iter.hasNext()){
+            iter.next().getInfo();
+        }
+
     }
 }
