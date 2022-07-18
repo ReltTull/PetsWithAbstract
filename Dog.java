e03026d9385fe912fe96423eab9c30680beb8ea1
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-18 19:13:49 +0300

    Reaction ierarchy edit

--- Dog.java
+++ Dog.java
@@ -2,22 +2,28 @@ public class Dog extends Pet implements Comparable<Pet>{
     String tailState;
 //    int distance;
 
+
+    @Override
+    void reaction() {
+        voice();
+        dogReaction();
+    }
+
     public Dog(int disToHuman) {
         distance = disToHuman;
     }
 
     @Override
     void voice() {
-        dogReaction();
+        System.out.println("Waf");
     }
     void dogReaction() {
         tailState = "wagging";
         distance -= 5;
-        System.out.println("Waf, " + tailState + ", " + distance);
     }
 
     @Override
     void getInfo() {
-        System.out.println("Dog, " + distance);
+        System.out.println("Dog, " + distance + ", " + tailState);
     }
 }
