e6e4ce38c89c2e400e4dc738df788ffba05a7733
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-18 19:14:44 +0300

    upd

--- Pet.java
+++ Pet.java
@@ -2,6 +2,11 @@ import java.util.Iterator;
 
 abstract class Pet implements Comparable<Pet>{
     int distance;
+
+    /**
+     * Метод включает в себя вызов метода voice(), так как голос - часть реакции
+     */
+    abstract void reaction();
     abstract void voice();
 
     abstract void getInfo();
